eb71a6cda5ccfdf65023ac7bd337521b30baaba1
Administrator <Administrator@20180914-120504>
2019-01-04 17:09:33 +0800

    # 10个实体类  pom.xml配置spring相关jar

---
+++ src/main/java/com/lym/entity/LocalAuth.java
@@ -0,0 +1,68 @@
+package com.lym.entity;
+
+import java.util.Date;
+
+public class LocalAuth {
+
+	private Long localAuthId;
+	
+	private String username;
+	
+	private String password;
+	
+	private Date CreateTime;
+	
+	private Date lastEditTime;
+	
+	private PersonInfo personInfo;
+
+	public Long getLocalAuthId() {
+		return localAuthId;
+	}
+
+	public void setLocalAuthId(Long localAuthId) {
+		this.localAuthId = localAuthId;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Date getCreateTime() {
+		return CreateTime;
+	}
+
+	public void setCreateTime(Date createTime) {
+		CreateTime = createTime;
+	}
+
+	public Date getLastEditTime() {
+		return lastEditTime;
+	}
+
+	public void setLastEditTime(Date lastEditTime) {
+		this.lastEditTime = lastEditTime;
+	}
+
+	public PersonInfo getPersonInfo() {
+		return personInfo;
+	}
+
+	public void setPersonInfo(PersonInfo personInfo) {
+		this.personInfo = personInfo;
+	}
+	
+	
+}
---
+++ src/main/java/com/lym/entity/ProductImg.java
@@ -0,0 +1,69 @@
+package com.lym.entity;
+
+import java.util.Date;
+
+public class ProductImg {
+
+	private Long productImgId;
+	
+	private String imgAddr;
+	
+	private String imgDesc;
+	
+	private Integer priority;
+	
+	private Date createtime;
+	
+	private Long productId;
+
+	public Long getProductImgId() {
+		return productImgId;
+	}
+
+	public void setProductImgId(Long productImgId) {
+		this.productImgId = productImgId;
+	}
+
+	public String getImgAddr() {
+		return imgAddr;
+	}
+
+	public void setImgAddr(String imgAddr) {
+		this.imgAddr = imgAddr;
+	}
+
+	public String getImgDesc() {
+		return imgDesc;
+	}
+
+	public void setImgDesc(String imgDesc) {
+		this.imgDesc = imgDesc;
+	}
+
+	public Integer getPriority() {
+		return priority;
+	}
+
+	public void setPriority(Integer priority) {
+		this.priority = priority;
+	}
+
+	public Date getCreatetime() {
+		return createtime;
+	}
+
+	public void setCreatetime(Date createtime) {
+		this.createtime = createtime;
+	}
+
+	public Long getProductId() {
+		return productId;
+	}
+
+	public void setProductId(Long productId) {
+		this.productId = productId;
+	}
+	
+	
+	
+}
