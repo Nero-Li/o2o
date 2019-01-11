163fc6dc3acae1369dfcf184daf102bef7dc3826
Nero_Li <375892799@qq.com>
2019-01-12 00:15:00 +0800

    #查询店铺列表 Dao层 + ut

--- src/main/java/com/lym/dao/ShopDao.java
+++ src/main/java/com/lym/dao/ShopDao.java
@@ -1,6 +1,9 @@
 package com.lym.dao;
 
 import com.lym.entity.Shop;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 /**
   * 店铺Dao
@@ -8,6 +11,24 @@ import com.lym.entity.Shop;
  *
  */
 public interface ShopDao {
+
+    /**
+     * 分页查询店铺,可输入的条件:店铺名(模糊),店铺状态,店铺类别,区域id,owner
+     *
+     * @param shopCondition
+     * @param rowIndex      从第几行开始取数据
+     * @param pageSize      每页大小
+     */
+    List<Shop> queryShopList(@Param("shopCondition") Shop shopCondition, @Param("rowIndex") int rowIndex,
+                             @Param("pageSize") int pageSize);
+
+    /**
+     * 返回queryShopList总数
+     *
+     * @param shopCondition
+     * @return
+     */
+    Integer queryShopCount(@Param("shopCondition") Shop shopCondition);
 	
 	/**
 	  * 新增店铺
--- src/test/java/com/lym/dao/ShopDaoTest.java
+++ src/test/java/com/lym/dao/ShopDaoTest.java
@@ -11,71 +11,86 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Date;
+import java.util.List;
 
-public class ShopDaoTest extends BaseTest{
-	
-	
-	private Logger logger = LoggerFactory.getLogger(ShopDaoTest.class);
-	
-	@Autowired
-	private ShopDao shopDao;
-	
-	@Test
-	public void testInsertShop() {
-		Shop shop = new Shop();
-		PersonInfo owner = new PersonInfo();
-		Area area = new Area();
-		ShopCategory shopCategory = new ShopCategory();
-		owner.setUserId(1L);
-		area.setAreaId(2);
-		shopCategory.setShopCategoryId(1L);
-		shop.setOwner(owner);
-		shop.setArea(area);
-		shop.setShopCategory(shopCategory);
-		shop.setShopName("测试的店铺");
-		shop.setShopDesc("testDesc");
-		shop.setPhone("testPhone");
-		shop.setShopAddr("testAddr");
-		shop.setShopImg("testImg");
-		shop.setPriority(1);
-		shop.setCreateTime(new Date());
-		shop.setEnableStatus(1);
-		shop.setAdvice("审核中");
-		shopDao.insertShop(shop);
+public class ShopDaoTest extends BaseTest {
 
-	}
 
-	@Test
-	public void testUpdateShop() {
-		Shop shop = new Shop();
-		PersonInfo owner = new PersonInfo();
-		Area area = new Area();
-		ShopCategory shopCategory = new ShopCategory();
-		owner.setUserId(1L);
-		area.setAreaId(2);
-		shop.setShopId(1L);
-		shopCategory.setShopCategoryId(1L);
-		shop.setOwner(owner);
-		shop.setArea(area);
-		shop.setShopCategory(shopCategory);
-		shop.setShopName("测试的店铺3");
-		shop.setShopDesc("testDesc");
-		shop.setPhone("testPhone");
-		shop.setShopAddr("testAddr");
-		shop.setShopImg("testImg");
-		shop.setPriority(1);
-		shop.setCreateTime(new Date());
-		shop.setEnableStatus(1);
-		shop.setAdvice("审核中");
-		shopDao.updateShop(shop);
-	}
-	
-	@Test
-	public void testQueryByShopId() {
-		logger.info("=========start========");
-		Shop shop = shopDao.queryByShopId(17L);
-		logger.info("=========end========");
-		
-	}
+    private Logger logger = LoggerFactory.getLogger(ShopDaoTest.class);
+
+    @Autowired
+    private ShopDao shopDao;
+
+    @Test
+    public void testInsertShop() {
+        Shop shop = new Shop();
+        PersonInfo owner = new PersonInfo();
+        Area area = new Area();
+        ShopCategory shopCategory = new ShopCategory();
+        owner.setUserId(1L);
+        area.setAreaId(2);
+        shopCategory.setShopCategoryId(1L);
+        shop.setOwner(owner);
+        shop.setArea(area);
+        shop.setShopCategory(shopCategory);
+        shop.setShopName("测试的店铺");
+        shop.setShopDesc("testDesc");
+        shop.setPhone("testPhone");
+        shop.setShopAddr("testAddr");
+        shop.setShopImg("testImg");
+        shop.setPriority(1);
+        shop.setCreateTime(new Date());
+        shop.setEnableStatus(1);
+        shop.setAdvice("审核中");
+        shopDao.insertShop(shop);
+
+    }
+
+    @Test
+    public void testUpdateShop() {
+        Shop shop = new Shop();
+        PersonInfo owner = new PersonInfo();
+        Area area = new Area();
+        ShopCategory shopCategory = new ShopCategory();
+        owner.setUserId(1L);
+        area.setAreaId(2);
+        shop.setShopId(1L);
+        shopCategory.setShopCategoryId(1L);
+        shop.setOwner(owner);
+        shop.setArea(area);
+        shop.setShopCategory(shopCategory);
+        shop.setShopName("测试的店铺3");
+        shop.setShopDesc("testDesc");
+        shop.setPhone("testPhone");
+        shop.setShopAddr("testAddr");
+        shop.setShopImg("testImg");
+        shop.setPriority(1);
+        shop.setCreateTime(new Date());
+        shop.setEnableStatus(1);
+        shop.setAdvice("审核中");
+        shopDao.updateShop(shop);
+    }
+
+    @Test
+    public void testQueryByShopId() {
+        logger.info("=========start========");
+        Shop shop = shopDao.queryByShopId(17L);
+        logger.info("=========end========");
+
+    }
+
+    @Test
+    public void testQueryShopListAndCount() {
+        Shop shopCondition = new Shop();
+        PersonInfo owner = new PersonInfo();
+        owner.setUserId(1L);
+        shopCondition.setOwner(owner);
+        ShopCategory shopCategory = new ShopCategory();
+        shopCategory.setShopCategoryId(2L);
+        shopCondition.setShopCategory(shopCategory);
+        List<Shop> shopList = shopDao.queryShopList(shopCondition, 0, 5);
+        int count = shopDao.queryShopCount(shopCondition);
+        System.out.println("当前店铺列表数量:" + shopList.size() + "===店铺总数:" + count);
+    }
 
 }
