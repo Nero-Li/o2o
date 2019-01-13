343248c100c3e8774b77e04458241dfdfa60b972
Nero_Li <375892799@qq.com>
2019-01-13 23:24:31 +0800

    # 店铺列表前后端联调通过

--- src/main/java/com/lym/controller/shopadmin/ShopAdminController.java
+++ src/main/java/com/lym/controller/shopadmin/ShopAdminController.java
@@ -4,13 +4,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+/**
+ * 因为html放在WEB-INF里,不能直接访问,需要通过SpringMVC路由
+ */
 @Controller
 @RequestMapping(value="shopadmin",method=RequestMethod.GET)
 public class ShopAdminController {
-	
-	@RequestMapping("/shopoperation")
-	public String shopOperation() {
-		return "shop/shopoperation";
-	}
+
+    @RequestMapping("/shopoperation")
+    public String shopOperation() {
+        return "shop/shopoperation";
+    }
+
+    @RequestMapping("/shoplist")
+    public String shopList() {
+        return "shop/shoplist";
+    }
 
 }
